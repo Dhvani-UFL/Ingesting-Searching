1dc7b5557c09ab92dd2c99c40c65ea7d2ac03bf4
divchag <chagantidivya@gmail.com>
2014-12-11 14:22:31 -0500

    Recommendation test cases

--- Dhvani/src/Dhvani/Test/RecommendationTest.java
+++ Dhvani/src/Dhvani/Test/RecommendationTest.java
@@ -1,5 +1,59 @@
 package Dhvani.Test;
 
+
+import Dhvani.*;
+import org.junit.*; 
+
 public class RecommendationTest {
+	
+	@Test
+    public void testingmethod0()
+    {
+        Recommendation mywrapper = new Recommendation();
+        
+        String filepath = "E:/Songs/MJ/You Rock My World.mp3";
+        try
+        {
+            mywrapper.RecommendSong(filepath);
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        
+    }
+	
+	@Test
+    public void testingmethod1()
+    {
+		Recommendation mywrapper = new Recommendation();
+        
+        String filepath = "E:/Songs/Eminem/Real Slim Shady.mp3";
+        try
+        {
+            mywrapper.RecommendSong(filepath);
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+    
+    @Test
+    public void testingmethod2()
+    {
+    	Recommendation mywrapper = new Recommendation();
+        
+        String filepath = "E:/Songs/Eminem/Superman.mp3";
+        try
+        {
+            mywrapper.RecommendSong(filepath);
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
 
 }
